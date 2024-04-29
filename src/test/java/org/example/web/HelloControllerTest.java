8dd1771c09a46bbfa1273e87e567487b2b32a668
yorer <jdaddyorer@gmail.com>
2024-04-29 15:55:00 +0900

    first

---
+++ src/test/java/org/example/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package org.example.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class) // 실행할 테스트 케이스 클래스를 지정하는데 사용
+@WebMvcTest(controllers = HelloController.class) //  Spring MVC 컨트롤러를 테스트하는 데 사용
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void sayHello() throws Exception{
+        String hello = "hello";
+        // MockMvc: Spring MVC 테스트를 위한 클래스로,
+        // 실제 HTTP 요청을 수행하지 않고도 컨트롤러의 동작을 시뮬레이션하고 검증할 수 있는 방법을 제공.
+        // andExpect(): 요청에대한 응답을 검증 해주는 메서드
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+}
