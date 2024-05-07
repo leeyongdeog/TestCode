e1a6bfe41901413e9bf751e14b8a3a8a35969fa6
jdaddy <jdaddyorer@gmail.com>
2024-05-07 10:54:15 +0900

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/app/step1
+PROJECT_NAME=TestCode
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull..."
+git pull origin yorer
+
+echo "> Start Build..."
+./gradlew build
+
+echo "> cd step1..."
+cd $REPOSITORY
+
+echo "> Copy jar To step1 folder..."
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> Check exist PID..."
+CURRENT_PID=${pgrep -f ${PROJECT_NAME}.*.jar}
+
+echo "> Current Running PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> Not exist Running PID"
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy Application..."
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+
