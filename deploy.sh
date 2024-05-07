8fc9f9e52e52153627fe56fe18ec0c54941367ac
jdaddy <jdaddyorer@gmail.com>
2024-05-07 17:26:09 +0900

    sh

--- deploy.sh
+++ deploy.sh
@@ -41,11 +41,11 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 nohup java -jar \
+-Dspring.profiles.active=real \
 -Dspring.config.location=classpath:/application.properties \
 -Dspring.config.location=classpath:/application-real.properties \
 -Dspring.config.location=/home/ubuntu/app/application-oauth.properties \
 -Dspring.config.location=/home/ubuntu/app/application-real-db.properties \
--Dspring.profiles.active=real \
 $REPOSITORY/$JAR_NAME 2>&1 &
 
 echo "Done!!"
